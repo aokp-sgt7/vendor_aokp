cf30e7a2c012e1baba8e13d7f9d3c436c9ab50c4
stimpz0r <stimpz0r@kritical.co>
2012-09-23 21:56:32 +1000

    ... and these! :(

---
+++ products/p1l.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, device/samsung/p1/full_p1l.mk)
+
+$(call inherit-product, vendor/aokp/configs/common_tablet_small.mk)
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/p1
+
+PRODUCT_NAME := aokp_p1l
+PRODUCT_DEVICE := p1l
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := GT-P1000L
+PRODUCT_RELEASE_NAME := P1L
+
+PRODUCT_COPY_FILES +=  \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_600_1024.zip:system/media/bootanimation.zip
