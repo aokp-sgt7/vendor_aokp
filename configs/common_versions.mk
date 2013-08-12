4fc0d019b2ebdc2859f02fc071e51bd2dd144f92
stimpz0r <stimpz0r@kritical.co>
2013-08-12 21:44:45 +1000

    FIXED: use ro.cm.device instead of ro.aokp.device - also set it in default.prop, not build.prop - this ensures people flashing from / back to CM won't get bootloops due to incorrect kernel flashing

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -6,11 +6,14 @@ BUILD_DEVICE = $(shell vendor/aokp/tools/getdevice)
 AOKP_BRANCH=jb-mr1
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.aokp.device=$(BUILD_DEVICE) \
     ro.aokp.version=aokp_sgt7-$(BUILD_DEVICE)-$(AOKP_BRANCH)_$(DATE) \
     ro.goo.developerid=stimpz0r \
     ro.goo.rom=aokpsgt7-jb \
     ro.goo.version=$(shell date +%s)
+
+# we use ro.cm.device to keep it compatible with CM roms - incase someone is coming from (or going back to) a CM variant...
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.cm.device=$(BUILD_DEVICE)
 
 # needed for statistics
 PRODUCT_PROPERTY_OVERRIDES += \
